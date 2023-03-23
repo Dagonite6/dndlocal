a9ab6b0003875f9261b77c575f4a9e9f657df435
vlad <vlad@vlads-MBP.netis.cc>
2023-03-23 15:33:18 +0100

    rest api

--- dnd/game/urls.py
+++ dnd/game/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.urls import path
-from . import controller
+from . import controller, views
 
 
 urlpatterns = [
     path('', controller.index),
+    path('character/', views.CharacterListCreate.as_view())
 ]
--- dnd/game/views.py
+++ dnd/game/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+from .models import Character
+from .serializers import CharacterSerializer
+from rest_framework import generics
+
+class CharacterListCreate(generics.ListCreateAPIView):
+    queryset = Character.objects.all()
+    serializer_class = CharacterSerializer
